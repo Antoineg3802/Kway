174cb8737dd419a0b27c4fe08b83aa4fb10b7c57
OMAILLOT <orian.maillot@ynov.com>
2022-06-02 17:14:16 +0200

    feat: Show dialogue with textForm

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,32 @@ class _MyHomePageState extends State<MyHomePage> {
                       style: ElevatedButton.styleFrom(
                         textStyle: const TextStyle(fontSize: 20),
                       ),
-                      onPressed: () {},
+                      onPressed: () => showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Ajouter une ville'),
+                          contentPadding:
+                              const EdgeInsets.only(top: 25.0, left: 5.0),
+                          buttonPadding: const EdgeInsets.all(5.0),
+                          content: const Text(
+                            "ville:",
+                            textAlign: TextAlign.left,
+                          ),
+                          actions: <Widget>[
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                hintText: 'Entrer votre ville',
+                              ),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
                       child: const Text('Ajouter une ville'),
                     ),
                   )
