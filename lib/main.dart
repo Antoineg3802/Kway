733b1f3862bda6395888322670d45daaa1f28925
Antoineg3802 <antoine.guerin@ynov.com>
2022-06-07 17:27:28 +0200

    feature : front done but need to merge for the listviews

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,235 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body : null,
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.fromLTRB(0, 70, 0, 0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Text(
+                  'Town',
+                  style: TextStyle(fontSize: 25),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 350,
+                  height: 200,
+                  padding: const EdgeInsets.all(10.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: Colors.blue[300],
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 149,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment : CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  const Text(
+                                    'Day', 
+                                    style:TextStyle(color: Colors.white),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  const Text(
+                                    'Date', 
+                                    style:TextStyle(color: Colors.white),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  const Text(
+                                    'Hour', 
+                                    style:TextStyle(color: Colors.white),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.only(left: 17.0, top: 17.0),
+                                    padding: const EdgeInsets.all(10.0),
+                                    width: 130,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue[300],
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        width: 1,
+                                      ),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child : Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        Image.network(
+                                          'https://cdn-icons-png.flaticon.com/512/1163/1163661.png',
+                                          width: 40.0,
+                                          height: 40.0,
+                                        ),
+                                        const Text(
+                                          'Weather',
+                                          style:TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 149,
+                            child: Column(
+                              crossAxisAlignment : CrossAxisAlignment.center,
+                              mainAxisAlignment : MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Text(
+                                  'XX.XX°', 
+                                  style:TextStyle(color: Colors.white,fontSize: 25),
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text(
+                                  'Humidity : XX %', 
+                                  style:TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text(
+                                  'Wind : XX.XX km/h', 
+                                  style:TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                // Expanded(
+                //   child: ListView(
+                //   scrollDirection: Axis.horizontal,
+                //   shrinkWrap: true,
+                //   children: const <Widget>[
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //     Card(
+                //       child : Text('Hour'),  
+                //     ),
+                //   ],
+                // ),
+                // ),
+              ],
+            ),
+            Row(
+              children : <Widget>[
+                ListView(
+                  scrollDirection: Axis.horizontal,
+                  children : const <Widget>[
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                    // Card(
+                    //   child : Text('Hour'),  
+                    // ),
+                  ],
+                ),
+              ]
+            ),
+          ],
+        ),
+      ),
+        // decoration: const BoxDecoration(
+        //   image: DecorationImage(
+        //     image: AssetImage("assets/img/troll-face.png"),
+        //     fit: BoxFit.cover,
+        //   ),
+        // ), 
     );
   }
 }
